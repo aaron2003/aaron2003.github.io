1e7d12cd052c55a37dbd9cc89366f508d9892267
aaron2003 <47902353+aaron2003@users.noreply.github.com>
2019-03-03 15:23:57 +0200

    Update MouseState.js

--- MouseState.js
+++ MouseState.js
@@ -2,18 +2,25 @@ function MouseState()
 {
 	this.Draw = function()
 	{
-		if(hit)
+		if(started && !pause)
 		{
+			if(hit)
+			{
+				push();
+				translate(mouseX - 13, mouseY - 13);
+				rotate(45);
+				image(mouse_im_1, 0, 0, 26, 26, 50, 43, 132, 132);
+				pop();
+				return;
+			}
 			push();
-			translate(mouseX - 13, mouseY - 13);
-			rotate(45);
-			image(mouse_im_1, 0, 0, 26, 26, 50, 43, 132, 132);
+			translate(mouseX - 10, mouseY - 10);
+			image(mouse_im_0, 0, 0, 20, 20, 50, 43, 132, 132);
 			pop();
-			return;
 		}
-		push();
-		translate(mouseX - 10, mouseY - 10);
-		image(mouse_im_0, 0, 0, 20, 20, 50, 43, 132, 132);
-		pop();
+		else
+		{
+			image(mouse_im_2 , mouseX, mouseY, 16, 16, 12, 12, 46, 46);
+		}
 	}
 }
